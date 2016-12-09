e5ba59da785d4a24ef1caf768a27e95b62095c7e
James Newman <jamesmnewman1@gmail.com>
2016-12-09 09:24:16 -0800

    a change

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var arr = [1, 2, 3, 4, 5];
+
 $(document).ready(function() {
-	console.log("hello, world!");
+	for (var i = 0; i < arr.length; i++) {
+		console.log("hello world " + i + " times");
+	}
 });
